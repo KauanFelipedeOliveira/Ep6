0bf978b849c5de68ca263cbbe37d4be332a8a47c
Nokaute <128047270+KauanFelipedeOliveira@users.noreply.github.com>
2023-10-26 08:46:58 -0300

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,4 +1,4 @@
-var imagem = document.getElementById("imagem");
+var imagem = document.getElementById("img-interativa");
 
 function mudarImagem(){
     imagem.src ="imagens/dois.jpg";
@@ -25,47 +25,101 @@ function funcao_resposta(){
     var resp2 = document.querySelector('input[name="resposta_2"]:checked');
     var resp3 = document.querySelector('input[name="resposta_3"]:checked');
     var resp4 = document.querySelector('input[name="resposta_4"]:checked');
+    var resp5 = document.querySelector('input[name="resposta_5"]:checked');
+    var resp6 = document.querySelector('input[name="resposta_6"]:checked');
+    var resp7 = document.querySelector('input[name="resposta_7"]:checked');
+    var resp8 = document.querySelector('input[name="resposta_8"]:checked');
+    var resp9 = document.querySelector('input[name="resposta_9"]:checked');
+    var resp10 = document.querySelector('input[name="resposta_10"]:checked');
 
     var contador = 0;
 
 
     if(resp1 && resp2 && resp3 && resp4 && resp5 && resp6 && resp7 && resp8 && resp9 && resp10){
         if(resp1.value == "correta"){
-            document.getElementById("resp_correta_1").innerHTML = "Correto!"
+            document.getElementById("resp_correta_1").innerHTML = "Correto!";
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_1").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+            document.getElementById("resp_errada_1").innerHTML = "Errada!";
 
         }
         if(resp2.value == "correta"){
-            document.getElementById("resp_correta_2").innerHTML = "Correto!"
+            document.getElementById("resp_correta_2").innerHTML = "Correto!";
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_2").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+            document.getElementById("resp_errada_2").innerHTML = "Errada!";
 
         }
         if(resp3.value == "correta"){
-            document.getElementById("resp_correta_3").innerHTML = "Correto!"
+            document.getElementById("resp_correta_3").innerHTML = "Correto!";
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_3").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+            document.getElementById("resp_errada_3").innerHTML = "Errada!";
 
         }
         if(resp4.value == "correta"){
-            document.getElementById("resp_correta_4").innerHTML = "Correto!"
+            document.getElementById("resp_correta_4").innerHTML = "Correto!";
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_4").innerHTML = "Errada, pois a soma dos quadrados dos catetos é igual ao quadrada da hipotenusa!"
+            document.getElementById("resp_errada_4").innerHTML = "Errada!";
+
+        }
+        if(resp5.value == "correta"){
+            document.getElementById("resp_correta_5").innerHTML = "Correto!";
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_5").innerHTML = "Errada!";
+
+        }
+        if(resp6.value == "correta"){
+            document.getElementById("resp_correta_6").innerHTML = "Correto!";
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_6").innerHTML = "Errada!";
+
+        }
+        if(resp7.value == "correta"){
+            document.getElementById("resp_correta_7").innerHTML = "Correto!";
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_7").innerHTML = "Errada!";
+
+        }
+        if(resp8.value == "correta"){
+            document.getElementById("resp_correta_8").innerHTML = "Correto!";
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_8").innerHTML = "Errada!";
+
+        }
+        if(resp9.value == "correta"){
+            document.getElementById("resp_correta_9").innerHTML = "Correto!";
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_9").innerHTML = "Errada!";
+
+        }
+        if(resp10.value == "correta"){
+            document.getElementById("resp_correta_10").innerHTML = "Correto!";
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_10").innerHTML = "Errada!";
 
         }
         document.getElementById("quantos_acertos").innerHTML = "Você acertou " + contador + " questões!";
     }
     else{
-        alert("não deixe questões em branco!")
+        alert("não deixe questões em branco!");
     }
 
 }
