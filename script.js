f37e84309103c9ea8a2246ecb1d5f2d168a7423f
Nokaute <128047270+KauanFelipedeOliveira@users.noreply.github.com>
2023-10-28 23:32:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,29 +9,37 @@ function checkAnswers() {
     const answer13 = parseFloat(document.getElementById('answer13').value);
     const results = document.getElementById('results');
 
-    const correctAnswers = {
-        question1: 500, // Resposta correta para a pergunta 1
-        question2: 50,  // Resposta correta para a pergunta 2
-        question3: 800   // Resposta correta para a pergunta 3
-    };
+    const correctAnswer = "3";
+    function exibirResposta() {
+        var resposta = document.getElementById("answer11").value;
+        
+        if(resposta.trim() !== "") {
+            if(resposta === correctAnswer) {
+                console.log("Resposta correta!");
+            }
+            else {
+                console.log("Resposta incorreta");
+            }
+        }
+    }
 
     results.innerHTML = "";
 
     if (!isNaN(answer11) && answer11 === correctAnswers.question1) {
         results.innerHTML += "Resposta 1: Correta!<br>";
     } else {
-        results.innerHTML += "Resposta 1: Incorreta. A resposta correta é 500.<br>";
+        results.innerHTML += "Resposta 1: Incorreta. A resposta correta é 3.<br>";
     }
 
     if (!isNaN(answer12) && answer12 === correctAnswers.question2) {
         results.innerHTML += "Resposta 2: Correta!<br>";
     } else {
-        results.innerHTML += "Resposta 2: Incorreta. A resposta correta é 50.<br>";
+        results.innerHTML += "Resposta 2: Incorreta. A resposta correta é -5.<br>";
     }
 
     if (!isNaN(answer13) && answer13 === correctAnswers.question3) {
         results.innerHTML += "Resposta 3: Correta!<br>";
     } else {
-        results.innerHTML += "Resposta 3: Incorreta. A resposta correta é 800.<br>";
+        results.innerHTML += "Resposta 3: Incorreta. A resposta correta é -2.<br>";
     }
 }
